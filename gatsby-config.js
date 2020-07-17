5e66b0a462f1616a53ef49caf5e3c6a2067263a7
florisdf <floris@hoezithet.nu>
2020-07-17 09:00:24 +0200

    Update Web App Manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,11 +20,11 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `Hoe Zit Het?`,
+                short_name: `hoezithet`,
                 start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
+                background_color: `#F4F4F4`,
+                theme_color: `#FFB700`,
                 display: `minimal-ui`,
                 icon: `src/images/favicon.png`, // This path is relative to the root of the site.
             },

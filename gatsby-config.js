4bf261c3ab52da9ededa849f3b7bb631a17fb351
florisdf <floris@hoezithet.nu>
2020-08-12 10:23:22 +0200

    Fix disaplay mode + unknown macros

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,6 @@ module.exports = {
                         resolve: `gatsby-remark-katex`,
                         options: {
                             // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
-                            strict: `ignore`,
                             macros: {
                                 "\\deg": "^{\\circ}",
                                 "\\norm": ["\\lVert#1\\rVert", 1],

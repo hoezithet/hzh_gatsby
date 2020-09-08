06978a2009dc7dc0fa9b0bbbdd6606bb6d37735f
Floris De Feyter <floris@hoezithet.nu>
2020-09-08 10:04:54 +0200

    Add empty home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from "../components/layout";
+import LayoutProps from "../components/layout";
+
+export default function Landing(props) {
+    return (
+        <Layout crumbs={[]}>
+        <h1>Hoe Zit Het?</h1>
+        </Layout>
+    );
+};

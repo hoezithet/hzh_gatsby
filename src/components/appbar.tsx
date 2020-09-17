4610203d624ffaa0cc64af432a452b01fd49e9c1
Floris De Feyter <floris@hoezithet.nu>
2020-09-17 12:19:18 +0200

    Move AppBar buttons under logo on small screens

--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -5,6 +5,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import logo from "../images/logo_header.png";
 
 const LogoImg = styled.img`
@@ -24,20 +26,31 @@ const LogoLink = styled(Link)`
     }
 `;
 
+const PageButtonsGrid = styled(Grid)`
+`;
+
 
 export default function HzhAppBar({ children }) {
     return (
         <AppBar position="static">
             <Toolbar>
-                <LogoLink href="/" color="inherit" variant="h5">
-                    <LogoImg src={logo} alt="Hoe Zit Het? logo"/>
-                    Hoe Zit Het?
-                </LogoLink>
-                <Button href="/lessen">Onze lessen</Button>
-                <span>|</span>
-                <Button href="/about">Wie zijn we?</Button>
-                <span>|</span>
-                <Button href="/trakteer">Trakteer ons!</Button>
+                <Grid container justify="space-between" alignItems="center">
+                    <Grid item xs={ 6 } md={ 6 }>
+                        <Box justifyContent="center">
+                            <LogoLink href="/" color="inherit" variant="h5">
+                                <LogoImg src={logo} alt="Hoe Zit Het? logo"/>
+                                Hoe Zit Het?
+                            </LogoLink>
+                        </Box>
+                    </Grid>
+                    <PageButtonsGrid item >
+                        <Button href="/lessen">Onze lessen</Button>
+                        <span>|</span>
+                        <Button href="/about">Wie zijn we?</Button>
+                        <span>|</span>
+                        <Button href="/trakteer">Trakteer ons!</Button>
+                    </PageButtonsGrid>
+                </Grid>
             </Toolbar>
         </AppBar>
    );

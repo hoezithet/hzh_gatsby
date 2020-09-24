65b6c898a3259d46a62b74bf74390dee998e6906
Floris De Feyter <floris@hoezithet.nu>
2020-09-24 18:19:33 +0200

    Add Over auteur to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,6 +10,7 @@ import demotivated from "../images/about/leerling_gedemotiveerd.png";
 import bfg_watch from "../images/about/HZH_GVR_watching.png";
 import bfg_run from "../images/about/HZH_GVR_run.png";
 import bfg_lift from "../images/about/HZH_GVR_lifter.png";
+import floris from "../images/about/floris.jpg";
 
 const StyledCite = styled.cite`
     color: ${COLORS.GRAY};
@@ -22,17 +23,25 @@ const AboutImg = styled.img`
     width: 75%;
 `;
 
+const ProfileImg = styled.img`
+    display: block;
+    margin: auto;
+    max-width: 100px;
+    width: 75%;
+    border-radius: 5px;
+`;
+
 
 export default function About() {
     return (
         <Layout>
-            <h1>Wie zijn we?</h1>
+            <h1>Over de website</h1>
             <StyledCite>
-                Het is zover. Morgen examen wiskunde. Ik heb al heel de namiddag en avond zitten studeren, maar het lijkt niet op te schieten. De stress neemt toe en ik begin wat paniekerig te bladeren door de cursus om te zien hoeveel ik nog moet studeren. Duidelijk meer dan verwacht. En dan heb ik eens de oefeningen nog niet gemaakt… Laat staan mijn toetsen opnieuw geprobeerd. Wat is dat toch met dat vak? Waarvoor ga ik dat trouwens ooit gebruiken? De moed zakt in mijn schoenen. Het licht gaat uit. Dit lukt nooit.
+                Het is zover. Morgen examen wiskunde. Ik heb al heel de namiddag en avond zitten studeren, maar het lijkt niet op te schieten. De stress neemt toe en ik begin wat paniekerig te bladeren door de cursus om te zien hoeveel ik nog moet studeren. Duidelijk meer dan verwacht. En dan heb ik eens de oefeningen nog niet gemaakt… Laat staan mijn toetsen opnieuw geprobeerd. Wat is dat toch met dat vak? Waarvoor ga ik dat trouwens ooit gebruiken? De moed zakt in mijn schoenen. Het licht gaat uit. Dit lukt nooit... 😞
         </StyledCite>
         <AboutImg src={ demotivated } />
         <p>
-            Misschien herken je het bovenstaande verhaal wel, net als veel leerlingen van het middelbaar. Bij Hoe Zit Het? zijn we <b>gepassioneerd door wiskunde en wetenschap</b>. Dat de vakken waar wij zo’n grote passie voor hebben zulke angst en paniek kunnen veroorzaken bij leerlingen, vinden we heel jammer. Het voelt alsof veel leerlingen bang zijn voor de Grote Vriendelijke Reus, maar <b>niemand hen ooit op zijn schouder heeft gezet</b>.
+            Misschien herken je het bovenstaande verhaal wel, net als veel leerlingen van het middelbaar. Mijn naam is Floris De Feyter 👋 en tussen ons gezegd en gezwegen vind ik wiskunde en wetenschappen eigenlijk heel interessant. Het doet wat pijn om te zien dat de vakken die ik zelf heel graag doe zulke angst en paniek kunnen veroorzaken bij leerlingen. Het voelt alsof veel leerlingen bang zijn voor de Grote Vriendelijke Reus, maar <b>niemand hen ooit op zijn schouder heeft gezet</b>.
         </p>
         <Grid container spacing={ 2 } justify="space-between">
             <Grid item xs={ 12 } sm={ 6 }>
@@ -44,14 +53,30 @@ export default function About() {
         </Grid>
         <h2>Wetenschap verlicht 💡</h2>
         <p>
-            Hoe Zit Het? wil het studeren van <b>wetenschap en wiskunde voor leerlingen verlichten</b>. We willen leerlingen op de schouder van de reus laten staan en hen tonen dat het uitzicht eigenlijk wel best mooi is. Wij willen niet dat leerstof wetenschap en wiskunde leerlingen nog langer slapeloze nachten bezorgt, maar hen een inwendig <b>vreugdesprongetje geeft bij elke nieuwe “Aha!”</b>.
+            Met Hoe Zit Het? wil ik het studeren van <b>wetenschap en wiskunde voor leerlingen verlichten</b>. Ik zou leerlingen op de schouder van de reus willen laten staan en hen tonen dat het uitzicht eigenlijk wel best mooi is. Leerstof wetenschap en wiskunde mag leerlingen geen slapeloze nachten bezorgen, vind ik, maar het moet hen een inwendig <b>vreugdesprongetje geven bij elke nieuwe “Aha!”</b>.
         </p>
         <p>
-            Wij willen leerlingen tonen dat <b>wetenschap verlicht</b>.
+            Met Hoe Zit Het? wil ik leerlingen tonen dat <b>wetenschap verlicht</b>.
         </p>
         <AboutImg src={ bfg_lift } />
-        <h2>Ons team</h2>
-        <h2>Wil jij ook meehelpen?</h2>
+        <h1>Over de auteur</h1>
+        <Grid container spacing={ 2 } justify="center">
+            <Grid item sx={ 12 } md={ 2 }>
+                <p>
+                <ProfileImg src={ floris } alt="Floris De Feyter, auteur van Hoe Zit Het?" />
+                </p>
+            </Grid>
+            <Grid item sx={ 12 } md={ 10 }>
+                <>
+                <p>
+                Van opleiding ben ik industrieel ingenieur elektronica-ICT en momenteel ben ik bezig aan een doctoraat over computervisie. Afgelopen jaren heb ik in mijn vrije tijd heel wat bijlessen wiskunde en fysica gegeven. Het leukste daaraan was om de blik van de leerlingen te zien opklaren eens <q>het lichtje ging branden</q>. Dat wil ik met Hoe Zit Het? ook graag bereiken door mijn uiterste best te doen om de leerstof helder uit te leggen aan de hand van sprekende illustraties.
+                </p>
+                <p>
+                Als je vragen of opmerkingen over de site hebt, mag je me altijd een mailtje sturen via <a href="mailto:floris@hoezithet.nu">floris@hoezithet.nu</a> of me contacteren via de <a href="https://fb.me/hoezithet">Facebookpagina van Hoe Zit Het?</a>.
+                </p>
+                </>
+            </Grid>
+        </Grid>
     </Layout>
     );
 }

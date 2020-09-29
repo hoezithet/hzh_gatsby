68d431fb56e57fb497b02b65d4dfc9d7add15d79
Floris De Feyter <floris@hoezithet.nu>
2020-09-29 09:30:03 +0200

    Add bokeh.tsx shortcode

---
+++ src/components/shortcodes/bokeh.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+interface BokehProps {
+    plot: string
+}
+
+export default function Bokeh(props: BokehProps) {
+    const { plot } = props;
+    const id = `${plot.replace(".json", "")}_${Math.random().toString(36).substring(2)}`;
+    return (
+        <>
+        <Helmet>
+            <link rel="stylesheet" href="https://cdn.pydata.org/bokeh/release/bokeh-1.0.2.min.css" type="text/css" />
+            <script type="text/javascript" src="https://cdn.pydata.org/bokeh/release/bokeh-1.0.2.min.js">
+            </script>
+            <script type="text/javascript" async
+                src="https://cdn.pydata.org/bokeh/release/bokeh-api-1.0.2.min.js">
+            </script>
+            <script type="text/javascript">
+            {`
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        var item = JSON.parse(this.responseText);
+                        if(typeof Bokeh !== 'undefined') {
+                            document.getElementById("${id}").innerHTML = '';
+                            Bokeh.embed.embed_item(item, "${ id }");
+                        }
+                    }
+                };
+                xmlhttp.open("GET", "${ plot }", true);
+                xmlhttp.send(); 
+            `}
+            </script>
+        </Helmet>
+        <div id={ id }>
+            <img src={ plot.replace(".json", ".png") } />
+        </div>
+        </>
+    );
+}

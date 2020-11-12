f809877aa1ddb058bf4b3594514331561babf6d2
Floris De Feyter <floris@hoezithet.nu>
2020-11-12 22:30:22 +0100

    Add sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     plugins: [
         `gatsby-plugin-typescript`,
         `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sitemap`,
         {
             resolve: `gatsby-source-filesystem`,
             options: {

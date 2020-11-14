e9d74dbf857a12fc1249f883c102bc1fdc1b1a10
Floris De Feyter <floris@hoezithet.nu>
2020-11-14 18:58:18 +0100

    Update site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,20 @@
 module.exports = {
     siteMetadata: {
+        organization: {
+            name: `Hoe Zit Het?`,
+            legalName: `Hoe Zit Het? vzw`,
+            founder: `Floris De Feyter`,
+            foundingDate: `2019`,
+            logo: `https://hoezithet.nu/images/favicon.png`,
+        },
         title: `Hoe Zit Het?`,
+        siteUrl: `https://hoezithet.nu`,
         description: `Verlichte uitleg wiskunde en fysica`,
-        author: `@hoezithet`,
-        siteURL: `https://hoezithet.nu`,
+        tags: [`uitleg`, `wiskunde`, `fysica`],
+        email: `hallo@hoezithet.nu`,
+        twitterUsername: `@flo_defeyter`,
+        socials: [`https://github.com/hoezithet`, `https://fb.me/hoezithet`],
+        lang: `nl`,
     },
     plugins: [
         `gatsby-plugin-typescript`,

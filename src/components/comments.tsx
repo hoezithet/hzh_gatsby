df5dcb52380cf09a1dfa17e6a2e2a3b82aa4d3a0
Floris De Feyter <floris@hoezithet.nu>
2020-11-19 23:02:52 +0100

    Create react-based Commento component

--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -1,17 +1,54 @@
-import React from "react";
+import React, {useEffect} from 'react';
 import { Helmet } from "react-helmet";
+
+// Helper to add scripts to our page
+const insertScript = (src, id, parentElement) => {
+  const script = window.document.createElement('script');
+  script.async = true;
+  script.src   = src;
+  script.id    = id;
+  script.setAttribute('data-no-fonts', "true");
+  script.setAttribute('data-css-override', "/css/commento.css");
+  parentElement.appendChild(script);
+
+  return script;
+};
+
+// Helper to remove scripts from our page
+const removeScript = (id, parentElement) => {
+  const script = window.document.getElementById(id);
+  if (script) {
+    parentElement.removeChild(script);
+  }
+};
+
+// The actual component
+const Commento = () => {
+  useEffect(() => {
+    // If there's no window there's nothing to do for us
+    if (! window) {
+      return;
+    }
+    const document = window.document;
+
+    // In case our #commento container exists we can add our commento script
+    if (document.getElementById('commento')) {
+      insertScript(`https://commento.hoezithet.nu/js/commento.js`, `commento-script`, document.body);
+    }
+
+    // Cleanup; remove the script from the page
+    return () => removeScript(`commento-script`, document.body);
+  });
+
+  return <div id={`commento`} />
+};
 
 export default function Comments() {
     return (
         <>
         <h2 id="comments">Vragen en reacties</h2>
-        <div id="commento"></div>
+        <Commento />
         <Helmet>
-            <script defer
-                src="https://commento.hoezithet.nu/js/commento.js"
-                data-no-fonts="true"
-                data-css-override="/css/commento.css">
-            </script>
             <script>
                 {`
                 var katexLoaded = false;

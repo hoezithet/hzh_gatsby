4f0cdcd6f9455d492b664ec94e32e29bad6a78a5
Floris De Feyter <floris@hoezithet.nu>
2020-12-24 13:09:11 +0100

    Refactor crumbs style

--- src/components/crumbs.tsx
+++ src/components/crumbs.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { Breadcrumbs } from '@material-ui/core';
+import { Breadcrumbs } from "@material-ui/core";
 import { Link } from "gatsby-theme-material-ui";
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 export interface CrumbProps {
     crumbs: {
@@ -16,26 +15,29 @@ const BreadcrumbLink = styled(Link)`
 `;
 
 const Crumbs = ({ crumbs }: CrumbProps) => {
-
     const breadCrumbLinks = crumbs.map(({ slug, title }, index) => {
         if (index < crumbs.length - 1) {
-            return (<BreadcrumbLink to={ slug } key={ slug }>
-                        { title }
-                    </BreadcrumbLink>);
+            return (
+                <BreadcrumbLink to={slug} key={slug}>
+                    {title}
+                </BreadcrumbLink>
+            );
         } else {
-            return (<BreadcrumbLink to={ slug } aria-current="page" key={ slug }>
-                        { title }
-                    </BreadcrumbLink>);
+            return (
+                <BreadcrumbLink to={slug} aria-current="page" key={slug}>
+                    {title}
+                </BreadcrumbLink>
+            );
         }
     });
 
     return (
         <header>
             <Breadcrumbs aria-label="breadcrumb" maxItems={2}>
-                { breadCrumbLinks }
+                {breadCrumbLinks}
             </Breadcrumbs>
         </header>
     );
-}
+};
 
 export default Crumbs;

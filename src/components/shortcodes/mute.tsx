40136d3961bccdd594d9bbda1db658031610a3f6
Floris De Feyter <floris@hoezithet.nu>
2020-12-28 18:21:37 +0100

    Add math rendering to mute shortcode

--- src/components/shortcodes/mute.tsx
+++ src/components/shortcodes/mute.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import COLORS from "../../colors";
 import styled from 'styled-components';
+import md2react from "../../utils/md2react";
 
 interface MuteProps {
     text: string;
@@ -8,11 +9,17 @@ interface MuteProps {
 
 const MuteSpan = styled.span`
     color: ${COLORS.GRAY};
-`
+    & > div {
+        display: inline;
+    }
+    & > div > p {
+        display: inline;
+    }
+`;
 
 const Mute = ({ text }: MuteProps) => (
     <MuteSpan>
-        {text}
+        {md2react(text)}
     </MuteSpan>
 );
 

800928d012520b027519106e5291d5e55201d5a6
Floris De Feyter <floris@hoezithet.nu>
2021-05-03 09:17:28 +0200

    Add hexToRGB function to colors

--- src/colors.js
+++ src/colors.js
@@ -31,4 +31,16 @@ const COLORS = {
     YELLOW: "#FFDE37",
 };
 
+export function hexToRGB(hex, alpha) {
+    var r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+
+    if (alpha) {
+        return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+    } else {
+        return "rgb(" + r + ", " + g + ", " + b + ")";
+    }
+}
+
 export default COLORS;

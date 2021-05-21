59c57c74ddfca355b3e9ecebfb2044e9615b274f
Floris De Feyter <floris@hoezithet.nu>
2021-05-21 10:10:33 +0200

    Remove unused sass from gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,6 @@ module.exports = {
                 path: `${__dirname}/src/images`,
             },
         },
-        `gatsby-plugin-sass`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {

437318087d1350e516937d782c3fa3543979774e
Floris De Feyter <floris@hoezithet.nu>
2021-05-26 06:40:18 +0200

    Fix gatsby-plugin-sitemap options

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
         {
             resolve: `gatsby-plugin-sitemap`,
             options: {
-                exclude: [`/bare/*`]
+                excludes: [`/bare/*`]
             }
         },
         {
